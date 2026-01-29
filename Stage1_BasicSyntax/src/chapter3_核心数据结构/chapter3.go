45549ad471eec927c18372b8837ddc45e1fe0050
Juvin <hdcqW@outlook.com>
2026-01-30 00:19:53 +0800

    commit study on 1.30

--- Stage1_BasicSyntax/src/chapter3_核心数据结构/chapter3.go
+++ Stage1_BasicSyntax/src/chapter3_核心数据结构/chapter3.go
@@ -141,8 +141,8 @@ Go 语言的取地址符是 &，放到一个变量前使用就会返回相应变
 一个指针变量指向了一个值的内存地址。
 类似于变量和常量，在使用指针前你需要声明指针。指针声明格式如下：
 var var_name *var-type
-var ip *int         指向整型 
-var fp *float32     指向浮点型 
+var ip *int         指向整型
+var fp *float32     指向浮点型
 
 2. 如何使用指针
 指针使用流程：
@@ -156,63 +156,165 @@ Go 空指针
 nil 指针也称为空指针。
 nil在概念上和其它语言的null、None、nil、NULL一样，都指代零值或空值。
 空指针判断：
-if(ptr != nil)     ptr 不是空指针 
-if(ptr == nil)     ptr 是空指针 
+if(ptr != nil)     ptr 不是空指针
+if(ptr == nil)     ptr 是空指针
 */
 
 func demo2() {
 	var a int = 42
-	var ip *int 
-	ip = &a 
-	fmt.Printf("a 变量的地址是: %x\n", &a  )
-    
+	var ip *int
+	ip = &a
+	fmt.Printf("a 变量的地址是: %x\n", &a)
+
 	/* 指针变量的存储地址 */
-    fmt.Printf("ip 变量储存的指针地址: %x\n", ip )
-    
+	fmt.Printf("ip 变量储存的指针地址: %x\n", ip)
+
 	/* 使用指针访问值 */
-    fmt.Printf("*ip 变量的值: %d\n", *ip )
-	
+	fmt.Printf("*ip 变量的值: %d\n", *ip)
+
 	// 空指针测试
-	var ptr *int 
-	fmt.Println("ptr的值为%x",ptr)
+	var ptr_ *int
+	fmt.Println("ptr的值为%x", ptr_)
 
 	/* 补充部分：指针数组 & 二级指针** & 向函数传递指针参数（同上） */
-	x, y, z := 7, 8, 9 
-	var ptrArr *int[3]
+	x, y, z := 7, 8, 9
+	var ptrArr [3]*int
 	ptrArr[0] = &x
-    ptrArr[1] = &y
-    ptrArr[2] = &z
+	ptrArr[1] = &y
+	ptrArr[2] = &z
 	for i := 0; i < 3; i++ {
 		fmt.Printf("ptrArr[%d] 指向的值是：%d\n", i, *ptrArr[i])
 	}
 	*ptrArr[0] = 999
-    fmt.Printf("修改后变量 x 的值: %d\n", x) // 输出 999
+	fmt.Printf("修改后变量 x 的值: %d\n", x) // 输出 999
 
 	// 二级指针
 	var ptr *int
-    var pptr **int  
-	ptr = &x      // ptr 存 a 的地址
-    pptr = &ptr   // pptr 存 ptr 的地址
+	var pptr **int
+	ptr = &x    // ptr 存 a 的地址
+	pptr = &ptr // pptr 存 ptr 的地址
 	fmt.Printf("变量 x = %d\n", a)
-    fmt.Printf("指针 ptr 指向的值 (*ptr) = %d\n", *ptr)
-    fmt.Printf("二级指针 pptr 最终指向的值 (**pptr) = %d\n", **pptr)
+	fmt.Printf("指针 ptr 指向的值 (*ptr) = %d\n", *ptr)
+	fmt.Printf("二级指针 pptr 最终指向的值 (**pptr) = %d\n", **pptr)
 	/* C++ 差异提示： 虽然 Go 支持二级指针，但在 Go 的实际业务开发（尤其是 Web 后端）中，极少会像 C++ 那样用到三级、四级指针。通常一级指针处理结构体传递就足够了。 */
 
 	// 向函数传递指针参数
-	f ,g = 100, 200
-	fmt.Println("Before Swap:") 
-	swap(&a, &b)
-	fmt.Println("After Swap(a,b):(%d,%d)",a, b) 
+	f, g := 100, 200
+	fmt.Println("Before Swap(f, g):(%d, %d)", f, g)
+	swap(&f, &g)
+	fmt.Println("After Swap(f, g):(%d, %d)", f, g)
+	/*
+		the same as
+		a, b = b, a
+	*/
+}
+
+func swap(x, y *int) {
+	temp := *x
+	*x = *y
+	*y = temp
 }
 
 /*--------------------------------------------------------------------------------------------------*/
 /*
 demo3 go语言结构体
+Go 语言中数组可以存储同一类型的数据，但在结构体中我们可以为不同项定义不同的数据类型。
+结构体是由一系列具有相同类型或不同类型的数据构成的数据集合。
+结构体表示一项记录，比如保存图书馆的书籍记录，每本书有以下属性：
+- Title ：标题
+- Author ： 作者
+- Subject：学科
+- ID：书籍ID
+
+1.定义结构体
+结构体定义需要使用 type 和 struct 语句。struct 语句定义一个新的数据类型，结构体中有一个或多个成员。type 语句设定了结构体的名称。结构体的格式如下：
+type struct_variable_type struct {
+   member definition
+   member definition
+   ...
+   member definition
+}
+
+一旦定义了结构体类型，它就能用于变量的声明，语法格式如下：
+variable_name := structure_variable_type {value1, value2...valuen}
+或
+variable_name := structure_variable_type { key1: value1, key2: value2..., keyn: valuen}
+
+2.访问结构体成员
+如果要访问结构体成员，需要使用点号 . 操作符，格式为：
+结构体.成员名"
+
+3.结构体作为函数参数
+你可以像其他数据类型一样将结构体类型作为参数传递给函数。
+
+4.结构体指针
+你可以定义指向结构体的指针类似于其他指针变量，格式如下：
+var struct_pointer *Books
+
+以上定义的指针变量可以存储结构体变量的地址。查看结构体变量地址，可以将 & 符号放置于结构体变量前：
+struct_pointer = &Book1
+
+使用结构体指针访问结构体成员，使用 "." 操作符：
+struct_pointer.title
 */
+type Book struct {
+	title   string
+	author  string
+	subject string
+	book_id int
+}
+
+func demo3() {
+	// 创建一个新的结构体
+	fmt.Println(Book{"Go_Study", "www.book.com", "Go_language", 1001})
+	// 也可以使用 key => value 格式
+	fmt.Println(Book{title: "Go 语言", author: "www.runoob.com", subject: "Go 语言教程", book_id: 6495407})
+	// 忽略的字段为 0 或 空
+	fmt.Println(Book{title: "Go 语言", author: "www.runoob.com"})
+
+	var Book1, Book2 Book
+	/* book 1 描述 */
+	Book1.title = "Go 语言"
+	Book1.author = "www.runoob.com"
+	Book1.subject = "Go 语言教程"
+	Book1.book_id = 6495407
+
+	/* book 2 描述 */
+	Book2.title = "Python 教程"
+	Book2.author = "www.runoob.com"
+	Book2.subject = "Python 语言教程"
+	Book2.book_id = 6495700
+
+	/* 打印 Book1 信息 */
+	printBook_(&Book1)
+
+	/* 打印 Book2 信息 */
+	printBook(Book2)
+}
+
+// 结构体作为函数参数,值传递
+func printBook(book Book) {
+	fmt.Printf("Book title : %s\n", book.title)
+	fmt.Printf("Book author : %s\n", book.author)
+	fmt.Printf("Book subject : %s\n", book.subject)
+	fmt.Printf("Book book_id : %d\n", book.book_id)
+}
+
+// 结构体指针作为函数参数
+func printBook_(book *Book) {
+	fmt.Printf("Book title : %s\n", book.title)
+	fmt.Printf("Book author : %s\n", book.author)
+	fmt.Printf("Book subject : %s\n", book.subject)
+	fmt.Printf("Book book_id : %d\n", book.book_id)
+}
 
 /*--------------------------------------------------------------------------------------------------*/
 /*
 demo4 go语言切片
+Go 语言切片是对数组的抽象。
+Go 数组的长度不可改变，在特定场景中这样的集合就不太适用，Go 中提供了一种灵活，功能强悍的内置类型切片("动态数组")，与数组相比切片的长度是不固定的，可以追加元素，在追加时可能使切片的容量增大。
+
+
 */
 
 /*--------------------------------------------------------------------------------------------------*/
