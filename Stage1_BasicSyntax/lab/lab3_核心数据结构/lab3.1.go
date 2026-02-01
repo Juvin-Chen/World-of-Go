5c73077b7d64c26fe6ae3e8b1861a6da95473f39
Juvin <hdcqW@outlook.com>
2026-02-02 01:06:02 +0800

    commit study on 2.1

--- Stage1_BasicSyntax/lab/lab3_核心数据结构/lab3.1.go
+++ Stage1_BasicSyntax/lab/lab3_核心数据结构/lab3.1.go
@@ -3,3 +3,20 @@ package main
 func main() {
 
 }
+
+func demo1() {
+	arr := [5]int{10, 20, 30, 40, 50}
+	doubleArrayByVal(arr)
+}
+
+// 错误示范：值传递
+func doubleArrayByVal(arr [5]int) {
+	for _, v := range arr {
+		v *= 2
+	}
+}
+
+// 正确示范：指针传递
+func task2() {
+
+}
